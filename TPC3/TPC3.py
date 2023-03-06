d0718d9fe37f70ccfbb92b68d309d3de942ec93a
andregoncalves02 <73440927+andregoncalves02@users.noreply.github.com>
2023-03-06 19:23:09 +0000

    Update TPC3.py

--- TPC3/TPC3.py
+++ TPC3/TPC3.py
@@ -9,7 +9,7 @@ def parser(file):
     for line in f:
         match = rexp.finditer(line)
         for m in match:
-            if match :
+            if match:
                 dicts = dicts + [m.groupdict()]
 
     return dicts
@@ -23,14 +23,39 @@ def freq_proc_ano(dicts):
         anos[dict["ano"]] += 1
     print("Frequencia de processos por ano: ")
     print(anos)
-def nomes(nome):
-    return re.match(r"/w+", nome), re.search(r"/w+$", nome)
 
+
+def nome_proprio(nome):
+    nomes = re.split(r"\s+", nome)
+    return nomes[0]
+
+
+def apelido(nome):
+    nomes = re.split(r"\s+", nome)
+    return nomes[len(nomes)-1]
+
+
+def top5_nomes(dicts, seculo):
+    top5_nomes = {}
+    top5_apelidos = {}
+    for dict in dicts:
+        if int(dict["ano"]) // 100 + 1 == seculo:
+            if nome_proprio(dict["nome"]) not in top5_nomes:
+                top5_nomes[nome_proprio(dict["nome"])] = 1
+            top5_nomes[nome_proprio(dict["nome"])] += 1
+            if apelido(dict["nome"]) not in top5_apelidos:
+                top5_apelidos[apelido("nome")] = 1
+            top5_apelidos[apelido("nome")] += 1
+    top5_nomes_ordenados = sorted(top5_nomes.items(), key=lambda x: x[1], reverse=True)[:5]
+    top5_apelidos_ordenados = sorted(top5_apelidos.items(), key=lambda x: x[1], reverse=True)[:5]
+    print(top5_nomes_ordenados)
+    print(top5_apelidos_ordenados)
 
 
 def main():
     dicts = parser("processos.txt")
     freq_proc_ano(dicts)
+    top5_nomes(dicts, 20)
 
 
 if __name__ == "__main__":
